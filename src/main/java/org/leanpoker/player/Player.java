cdc1b2114ec15a665dcb08d70d752e734dc5d7aa
Florin Macicasan <fmacicasan@sdl.com>
2015-04-25 11:42:13 +0300

    increased return to 200

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -2,17 +2,21 @@ package org.leanpoker.player;
 
 import com.google.gson.JsonElement;
 
+import java.io.IOException;
 import java.util.Map;
 
 public class Player {
 
-    static final String VERSION = "test version";
+    static final String VERSION = "b00ld away";
 
-    public static int betRequest(JsonElement request) {
+    protected static final Integer DEFAULT_RETURN = 200;
+
+    public static int betRequest(JsonElement request) throws IOException {
 
         System.out.println("request:" + request);
         System.out.println("Will return default");
-        return 100;
+
+        return DEFAULT_RETURN;
     }
 
     public static void showdown(JsonElement game) {
